97058ca08f51d3bdd085ffd70dd21b8a3dddd57e
Nenad005 <markonenadovic2005@gmail.com>
2024-12-01 04:38:11 +0100

    css promene

--- src/components/GamePage/Kartica.jsx
+++ src/components/GamePage/Kartica.jsx
@@ -5,10 +5,11 @@ export default function Kartica({tip, pitanje}){
     const [zavrsen, setZavrsen] = useState(false)
 
     return <>
-        <div style={{background: zavrsen ? "#363636" : "#FF5400"}}>
+        <div className={zavrsen? "zavrsen" : ""}>
             <h2>{pitanje}</h2>
             {tip == "3 za 5" && <Timer zavrsen={zavrsen} setZavrsen={setZavrsen}></Timer>}
             {tip != "3 za 5" && <>
+                <div id="zapocni" onClick={() => {setZavrsen(true)}}>{zavrsen ? "Gotovo" : "Zavrsi"}</div>
             </>}
         </div>
     </>
